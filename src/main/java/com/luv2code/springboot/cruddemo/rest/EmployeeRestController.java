25e9df5e571a111998c9b8055003bb25dc01f065
Lucas Couto <loc.unb@gmail.com>
2019-05-30 15:09:02 -0300

    Added endpoints to get and employee by id and save a new employee

--- src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +29,31 @@ public class EmployeeRestController {
 	@GetMapping("employees")
 	public List<Employee> findAll(){
 		return employeeService.findAll();
+	}
+	
+	// add mapping for GET /employees/{employeeId}
+	@GetMapping("employees/{employeeId}")
+	public Employee getEmployee(@PathVariable int employeeId) {
+		
+		Employee employee = employeeService.findById(employeeId);
+		
+		if(employee == null) {
+			throw new RuntimeException("Employee id not found - " + employeeId);
+		}
+		return employee;
+	}
+	
+	// add mapping for POST / employees - add new employee
+	@PostMapping("employees")
+	public Employee addEmployee(@RequestBody Employee employee) {
+		
+		// just in case they pass an id in JSON... set id to 0
+		// this is to force a save of new item... instead of update
+		employee.setId(0);
+		
+		employeeService.save(employee);
+
+		return employee;
 	}
 
 }
