dd43fb1e76a63df076d63f115931c98815e9ef0a
Lucas Couto <loc.unb@gmail.com>
2019-05-30 15:22:16 -0300

    Added endpoints to update and delete and employee

--- src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -3,9 +3,11 @@ package com.luv2code.springboot.cruddemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -55,5 +57,32 @@ public class EmployeeRestController {
 
 		return employee;
 	}
+	
+	// add mapping for PUT / employees - update existing employee
+	@PutMapping("employees")
+	public Employee updateEmployee(@RequestBody Employee employee) {
+	
+		employeeService.save(employee);
+		
+		return employee;
+	}
+	
+	// add mapping for DELETE /employees/{employeeId} - delete employee
+	@DeleteMapping("employees/{employeeId}")
+	public String deleteEmployee(@PathVariable int employeeId) {
+		
+		Employee employee = employeeService.findById(employeeId);
+		
+		// throw exception if null
+		
+		if(employee == null) {
+			throw new RuntimeException("employee id not found - " + employeeId);
+		}
+		
+		employeeService.deleteById(employeeId);
+		
+		return "Deleted employee id - " + employeeId;
+	}
+	
 
 }
